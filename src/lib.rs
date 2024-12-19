7d383ddccc7d2883aa89ee9702329718621135ad
Yosh <github@yosh.is>
2024-12-19 21:33:32 +0100

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+//! Convert dhat JSON output to a collapsed flamegraph format
+//!
+//! # Examples
+//!
+//! ```
+//! // tbi
+//! ```
+
+#![forbid(unsafe_code, rust_2018_idioms)]
+#![deny(missing_debug_implementations, nonstandard_style)]
+#![warn(missing_docs, future_incompatible, unreachable_pub)]
